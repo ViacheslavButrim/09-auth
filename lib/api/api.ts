38af138ff20f02f23f9dea936dca29979573eaf5
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2026-03-02 11:13:50 +0200

    wtf

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = '/api';
+const BASE_URL =  process.env.NEXT_PUBLIC_BASE_URL + '/api';
 
 export const nextServer = axios.create({
   baseURL: BASE_URL,
