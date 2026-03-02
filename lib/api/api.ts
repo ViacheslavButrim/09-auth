2af846a38cdd2a8213c1230c945e76fdef21f7e3
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2026-03-02 02:26:14 +0200

    hz

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://notehub-api.goit.study' + '/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
 export const api = axios.create({
   baseURL: BASE_URL,
