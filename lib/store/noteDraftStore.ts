99a14baa2f180b87cf6aae4862101dd50eb2ea64
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2026-03-03 23:35:08 +0200

    ftf

--- lib/store/noteDraftStore.ts
+++ lib/store/noteDraftStore.ts
@@ -8,31 +8,15 @@ interface NoteDraftStore {
   reset: () => void;
 }
 
-const validTags = [
-  "Work",
-  "Personal",
-  "Meeting",
-  "Shopping",
-  "Ideas",
-  "Travel",
-  "Finance",
-  "Health",
-  "Important",
-  "Todo",
-];
-
 export const useNoteDraftStore = create<NoteDraftStore>((set) => ({
   title: "",
   content: "",
   tag: "",
   setField: (field, value) =>
-    set((state) => ({
-      ...state,
-      [field]:
-        field === "tag"
-          ? validTags.find((t) => t.toLowerCase() === value.toLowerCase()) || ""
-          : value,
-    })),
+  set((state) => ({
+    ...state,
+    [field]: value, 
+  })),
   reset: () =>
     set({
       title: "",
